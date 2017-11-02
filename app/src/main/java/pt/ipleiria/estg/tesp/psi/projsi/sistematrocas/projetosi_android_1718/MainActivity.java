05385d8bd3daa0f152c324be6648c4d9aa174ddf
Leonardo <leonardo@MasterLeo>
2017-11-02 12:25:58 +0000

    Design Nav Drawer (uso da nav noutras atividades)

--- app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/MainActivity.java
@@ -3,15 +3,16 @@ package pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent teste = new Intent(this, HomePageActivity.class);
-        startActivity(teste);;
+        //setContentView(R.layout.activity_main);
+        View.inflate(this, R.layout.activity_main, (ViewGroup) findViewById(R.id.drawer_layout));
     }
 }
