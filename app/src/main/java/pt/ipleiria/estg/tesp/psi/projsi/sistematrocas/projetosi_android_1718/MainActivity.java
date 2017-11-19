3438984fb45c8c46e4cc1464c6e63b8287c526fb
Leonardo <leonardo@MasterLeo>
2017-11-19 22:12:01 +0000

    Design main page. +/- pronto

--- app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/MainActivity.java
@@ -1,5 +1,6 @@
 package pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718;
 
+import android.animation.LayoutTransition;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.List;
@@ -33,8 +35,42 @@ public class MainActivity extends NavDrawerActivity {
         //setContentView(R.layout.activity_main);
         View.inflate(this, R.layout.activity_main, (ViewGroup) findViewById(R.id.app_content));
 
+
+        LinearLayout layout = findViewById(R.id.layoutMain);
+        layout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        //Animar lvPropostas
+        View lblPropostas = findViewById(R.id.lblPropostas);
+
+        lblPropostas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewGroup.LayoutParams params = lvPropostas.getLayoutParams();
+                params.height = params.height == 0 ? ViewGroup.LayoutParams.WRAP_CONTENT : 0;
+                lvPropostas.setLayoutParams(params);
+            }
+        });
+
+        //Animar lvAnuncios
+        View lblAnuncios = findViewById(R.id.lblAnuncios);
+
+        lblAnuncios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewGroup.LayoutParams params = lvAnuncios.getLayoutParams();
+                params.height = params.height == 0 ? ViewGroup.LayoutParams.WRAP_CONTENT : 0;
+                lvAnuncios.setLayoutParams(params);
+            }
+        });
+
+
+
+
         /*if (savedInstanceState == null)
         {*/
+
+
+            //--------------
             gestorAnuncios = new GestorAnuncios();
             gestorPropostas = new GestorPropostas();
 
@@ -48,10 +84,12 @@ public class MainActivity extends NavDrawerActivity {
             //------
             anunciosAdapter = new AnunciosAdapter(this, gestorAnuncios.getListaAnuncios());
             lvAnuncios.setAdapter(anunciosAdapter);
+            //--------------
 
         /*}else{
             // this.gestorAnuncios = (GestorAnuncios) savedInstanceState.getSerializable(...);
             // this.gestorPropostas = (GestorPropostas) savedInstanceState.getSerializable(...);
         }*/
+
     }
 }
