48ba34e4d325b9ef0ff5ada2f967ee7e6ee9f863
João P <2160453@my.ipleiria.pt>
2017-12-05 12:27:13 +0000

    Design do Criar Anúncio

---
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/CriarAnuncioActivity.java
@@ -0,0 +1,29 @@
+package pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CriarAnuncioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_anuncio);
+        Toolbar toolbar = findViewById(R.id.toolbarCriarAnuncio);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fabCriarAnuncio = findViewById(R.id.fabCriarAnuncio);
+        fabCriarAnuncio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
