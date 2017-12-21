c18746dd5f535f4d02032d15e056fbb1163e8ae8
João P <2160453@my.ipleiria.pt>
2017-12-21 19:23:53 +0000

    População dos spinner e adição de number pickers

--- app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/CriarAnuncioActivity.java
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/CriarAnuncioActivity.java
@@ -5,6 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+
+import java.util.HashMap;
 
 public class CriarAnuncioActivity extends AppCompatActivity {
 
@@ -15,15 +21,81 @@ public class CriarAnuncioActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarCriarAnuncio);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fabCriarAnuncio = findViewById(R.id.fabCriarAnuncio);
-        fabCriarAnuncio.setOnClickListener(new View.OnClickListener() {
+        /*String[] keys = getResources().getStringArray(R.array.categorias_keys);
+        String[] values = getResources().getStringArray(R.array.categorias_values);
+
+        HashMap<String, String> map = new HashMap<>();
+
+        for (int contador = 0; contador < keys.length; contador++) {
+            map.put(keys[contador], values[contador]);
+        }*/
+
+        /*ArrayAdapter<HashMap<String, String>> spinnerAdapterTroco = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        spinnerAdapterTroco.add(map);*/
+
+        /*ArrayAdapter<HashMap<String, String>> spinnerAdapterPor = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        spinnerAdapterPor.add(map);*/
+
+
+
+        //Array Adapter das categorias guardadas nos recursos XML
+        ArrayAdapter<CharSequence> spinnerCategorias = ArrayAdapter.createFromResource(this,
+                R.array.categorias_values,
+                R.layout.custom_spinner_item);
+
+        Spinner dropDownCategoriasTroco = findViewById(R.id.dropDownCategoriasTroco);
+
+        spinnerCategorias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDownCategoriasTroco.setAdapter(spinnerCategorias);
+
+        dropDownCategoriasTroco.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
 
             @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Spinner dropDownCategoriasPor = findViewById(R.id.dropDownCategoriasPor);
+
+        spinnerCategorias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDownCategoriasPor.setAdapter(spinnerCategorias);
+
+        dropDownCategoriasPor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        NumberPicker numberPickerCategoriaTroco = findViewById(R.id.numberPickerCategoriaTroco);
+
+        numberPickerCategoriaTroco.setMinValue(1);
+        numberPickerCategoriaTroco.setMaxValue(99);
+        numberPickerCategoriaTroco.setWrapSelectorWheel(true);
+
+        NumberPicker numberPickerCategoriaPor = findViewById(R.id.numberPickerCategoriaPor);
+
+        numberPickerCategoriaPor.setMinValue(1);
+        numberPickerCategoriaPor.setMaxValue(99);
+        numberPickerCategoriaPor.setWrapSelectorWheel(true);
+
+        FloatingActionButton fabCriarAnuncio = findViewById(R.id.fabCriarAnuncio);
+
+        fabCriarAnuncio.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+
             }
         });
     }
-
 }
