8cb8cc512ff551e5731ba86beefcb1a941110b9f
João P <2160453@my.ipleiria.pt>
2017-12-25 00:14:14 +0000

    Selecionamento e Desselecionamento dos Forms Funcional

--- app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/CriarAnuncioActivity.java
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/CriarAnuncioActivity.java
@@ -1,6 +1,7 @@
 package pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718;
 
 import android.annotation.SuppressLint;
+import android.support.annotation.IdRes;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -20,7 +21,7 @@ import java.util.HashMap;
 
 import pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718.forms.FormSelector;
 
-public class CriarAnuncioActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class CriarAnuncioActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private HashMap<Integer, String> categoriasHashMap;
 
@@ -77,86 +78,25 @@ public class CriarAnuncioActivity extends AppCompatActivity implements AdapterVi
 
         //----------------------------------Botão Flutuante--------------------------------------
         FloatingActionButton fabCriarAnuncio = findViewById(R.id.fabCriarAnuncio);
-
-        fabCriarAnuncio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        fabCriarAnuncio.setOnClickListener(this);
         //---------------------------------------------------------------------------------------
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)  {
 
-        FormSelector formSelector = new FormSelector();
+        switch (parent.getId()) {
+            case R.id.dropDownCategoriasTroco:
 
-        FragmentManager manager = getSupportFragmentManager();
+                printForm(position, R.id.fragmentFormCategoriaTroco);
 
-        if(position > 0) {
+                break;
 
-            switch (parent.getId()) {
-                case R.id.dropDownCategoriasTroco:
+            case R.id.dropDownCategoriasPor:
 
-                    String categoriaTroco = categoriasHashMap.get(position);
+                printForm(position, R.id.fragmentFormCategoriaPor);
 
-                    try {
-
-                        Fragment form = formSelector.selectForm(categoriaTroco, getResources().getStringArray(R.array.categorias_keys),
-                                getApplicationContext());
-
-                        if(form != null) {
-                            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                            FrameLayout fragmentContainer = findViewById(R.id.fragmentFormCategoriaTroco);
-
-                            if (fragmentContainer.getChildCount() == 0) {
-                                transaction.add(R.id.fragmentFormCategoriaTroco, form, categoriaTroco).commit();
-                            } else {
-                                if (manager.findFragmentByTag(categoriaTroco) == null) {
-                                    transaction.replace(R.id.fragmentFormCategoriaTroco, form, categoriaTroco).commit();
-                                }
-                            }
-                        }
-
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                        showNotification();
-                    }
-
-                    break;
-
-                case R.id.dropDownCategoriasPor:
-
-                    String categoriaPor = categoriasHashMap.get(position);
-
-                        try {
-
-                            Fragment form = formSelector.selectForm(categoriaPor, getResources().getStringArray(R.array.categorias_keys),
-                                    getApplicationContext());
-
-                            if(form != null) {
-                                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                                FrameLayout fragmentContainer = findViewById(R.id.fragmentFormCategoriaPor);
-
-                                if (fragmentContainer.getChildCount() == 0) {
-                                    transaction.add(R.id.fragmentFormCategoriaPor, form, categoriaPor).commit();
-                                } else {
-                                    if (manager.findFragmentByTag(categoriaPor) == null) {
-                                        transaction.replace(R.id.fragmentFormCategoriaTroco, form, categoriaPor).commit();
-                                    }
-                                }
-                            }
-
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
-                            showNotification();
-                        }
-
-                    break;
-            }
+                break;
         }
     }
 
@@ -165,10 +105,64 @@ public class CriarAnuncioActivity extends AppCompatActivity implements AdapterVi
 
     }
 
-    private void showNotification() {
+    private void showNotification(String message) {
         Snackbar snackbar = Snackbar
-                .make(findViewById(R.id.coordinatorLayoutCriarAnuncio), "Ocorreu um erro na seleção da categoria", Snackbar.LENGTH_LONG);
+                .make(findViewById(R.id.coordinatorLayoutCriarAnuncio), message, Snackbar.LENGTH_LONG);
 
         snackbar.show();
+    }
+
+    private void printForm(Integer position, @IdRes int containerId) {
+
+        FormSelector formSelector = new FormSelector();
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        if(position > 0) {
+
+            String categoria = categoriasHashMap.get(position);
+
+            try {
+
+                Fragment form = formSelector.selectForm(categoria, getResources().getStringArray(R.array.categorias_keys),
+                        getApplicationContext());
+
+                if (form != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                    FrameLayout fragmentContainer = findViewById(containerId);
+
+                    if (fragmentContainer.getChildCount() == 0) {
+                        transaction.add(containerId, form, categoria).commit();
+                    } else {
+                        if (manager.findFragmentByTag(categoria) == null) {
+                            transaction.replace(containerId, form, categoria).commit();
+                        }
+                    }
+                }
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                showNotification("Ocorreu um erro na seleção da categoria");
+            }
+        } else {
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            FrameLayout fragmentContainer = findViewById(containerId);
+
+            if (fragmentContainer.getChildCount() != 0) {
+                Fragment fragment = manager.findFragmentById(containerId);
+
+                if(fragment != null) {
+                    transaction.remove(fragment).commit();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
