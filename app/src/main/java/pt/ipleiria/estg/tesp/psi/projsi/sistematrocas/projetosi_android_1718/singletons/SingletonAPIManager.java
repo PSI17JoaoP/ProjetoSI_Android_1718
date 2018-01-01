7657d052409359dbd59308156b43be5fb5e0cd56
Leonardo <leonardo@MasterLeo>
2018-01-01 10:59:58 +0000

    criar anuncio corrigido

--- app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/singletons/SingletonAPIManager.java
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/singletons/SingletonAPIManager.java
@@ -36,6 +36,7 @@ public class SingletonAPIManager {
     private SharedPreferences preferences;
 
     private static final String baseURL = "http://192.168.1.2:8888/";
+    //private static final String baseURL = "http://10.0.2.2:8888/";
     private static String auth = null;
 
     public static synchronized SingletonAPIManager getInstance(Context contexto) {
