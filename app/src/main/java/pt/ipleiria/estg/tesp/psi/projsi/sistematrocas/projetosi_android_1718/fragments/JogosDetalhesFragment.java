d982970fa7ffb2fd5008b2e4add0ecaadf9a97ea
João P <2160453@my.ipleiria.pt>
2018-01-20 23:09:02 +0000

    Continuação do commit anterior

---
+++ app/src/main/java/pt/ipleiria/estg/tesp/psi/projsi/sistematrocas/projetosi_android_1718/fragments/JogosDetalhesFragment.java
@@ -0,0 +1,25 @@
+package pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pt.ipleiria.estg.tesp.psi.projsi.sistematrocas.projetosi_android_1718.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JogosDetalhesFragment extends Fragment {
+
+    public JogosDetalhesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jogos_detalhes, container, false);
+    }
+}
